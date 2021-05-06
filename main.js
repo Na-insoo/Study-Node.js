b34e2df77370189981c0ac6c263127caf0e30af6
Na-insoo <fkvmzktm2@naver.com>
2021-05-06 12:34:40 +0900

    002-Query string 1
    requerst.url => query string 이였다!

--- main.js
+++ main.js
@@ -2,17 +2,15 @@ var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request,response){
     var url = request.url;
-    if(request.url == '/'){
+    console.log(url);
+    if(url == '/'){
       url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
-        return; response.writeHead(404);
-
-        
+    if(url == '/favicon.ico'){
+      return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url);
     response.end(fs.readFileSync(__dirname + url));
- 
+
 });
 app.listen(3000);
