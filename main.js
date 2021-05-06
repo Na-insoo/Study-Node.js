6e7aefdefda5dbd7f1ea4eeca0ae84e351f0d09c
Na-insoo <fkvmzktm2@naver.com>
2021-05-06 14:23:30 +0900

    006-Query string 5
    readFile를 통해서 파일을 불러보자 -> 적용

--- main.js
+++ main.js
@@ -17,8 +17,11 @@ var app = http.createServer(function(request,response){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    // 1.html를 그대로 복사해 왔다
-    var template = `
+    // readFile로 이용해서 data폴더 안에 있는 txt파일을 불러온다
+    // quertData.id = HTML 클릭하면 HTML, CSS 클릭하면 CSS
+    // txt파일명을 description 변수 자리에 배치된다. = txt 파일 본문이 배치된다.
+    fs.readFile(`data/${queryData.id}`,'utf8',function(err, description){
+        var template = `
         <!doctype html>
         <html>
         <head>
@@ -33,15 +36,15 @@ var app = http.createServer(function(request,response){
             <li><a href="/?id=JavaScript">JavaScript</a></li>
         </ul>
         <h2>${title}</h2>
-        <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-        <img src="coding.jpg" width="100%">
-        </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-        </p>
+        <p>${description}</p>
         </body>
         </html>
     `;
     // 웹페이지에 Query String 주소에다가 css를 적으면 css가 웹페이지에 뜨고 html로 고치면 html이 뜬다.
     response.end(template);
 
+    })
+   
+    
 });
 app.listen(3000);
