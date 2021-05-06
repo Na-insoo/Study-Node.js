f3b155fb1b098ca77cfc7aef891d1bbea5eb758f
Na-insoo <76979573+Na-insoo@users.noreply.github.com>
2021-05-06 11:37:34 +0900

    Update variable.js
    <img width="1331" alt="Screen Shot 2021-05-06 at 11 35 52 AM" src="https://user-images.githubusercontent.com/76979573/117234090-49e80280-ae5f-11eb-810d-14511f637789.png">

--- syntax/variable.js
+++ syntax/variable.js
@@ -5,4 +5,3 @@ var name = 'insoo'
 var letter = 'Lorem Ipsum  '+name+'  is simply  '+name+'  dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 console.log(letter);
 
-<img width="1331" alt="Screen Shot 2021-05-06 at 11 35 52 AM" src="https://user-images.githubusercontent.com/76979573/117234090-49e80280-ae5f-11eb-810d-14511f637789.png">
