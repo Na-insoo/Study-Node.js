64a89fadbe818236e5d3f1f4fa4cf09a99fcf822
Na-insoo <fkvmzktm2@naver.com>
2021-05-06 13:55:41 +0900

    004-Query string 3
    title 동적으로 만들기

--- main.js
+++ main.js
@@ -7,17 +7,41 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var title = queryData.id
     // queryDaya 객체 안에 id라는 키를 이용해 답을 얻는다
     console.log(queryData.id);
     if(_url == '/'){
-      _url = '/index.html';
+      title  = 'welcome';
     }
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
+    // 1.html를 그대로 복사해 왔다
+    var template = `
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+        <img src="coding.jpg" width="100%">
+        </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+        </p>
+        </body>
+        </html>
+    `;
     // 웹페이지에 Query String 주소에다가 css를 적으면 css가 웹페이지에 뜨고 html로 고치면 html이 뜬다.
-    response.end(queryData.id);
+    response.end(template);
 
 });
 app.listen(3000);
